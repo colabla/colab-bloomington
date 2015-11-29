eb19127eb54b67ccf32037541652e47154928301
George Witwer <george@colab.la>
2015-11-28 19:58:48 -0800

    in-progress text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	title: 'Express',
+  	pkg: {
+  		keywords: [
+  			'colab',
+  			'bloomington',
+  			'venture',
+  			'studio'
+  		],
+  		description: 'Colab Bloomington: A Colab Initiative'
+  	}
+  });
 });
 
 module.exports = router;
